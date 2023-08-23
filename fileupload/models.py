63b096fdad63f5409ada4cc49d0637c00b1b7cbf
strongmhk <kim6562166086@gmail.com>
2023-08-23 22:01:50 +0900

    refactor : Entity 이름 수정, 새로운 필드 추가

--- fileupload/models.py
+++ fileupload/models.py
@@ -3,9 +3,14 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 # Create your models here.
 
-class FileUpload(models.Model):
+# rawdata entity
+class RawData(models.Model):
+  host = models.TextField(max_length=10, null=False)
   title = models.TextField(max_length=40, null=True)
   imgfile = models.ImageField(null=True, upload_to="", blank=True)
   content = models.TextField()
+
+# output entity
+class Output(models.Model):
   
 
