312e4ed0a4510c312408471defe8b6835d4a7ff0
strongmhk <kim6562166086@gmail.com>
2023-09-06 23:29:53 +0900

    migrate: output 스키마 변경(analysis_tech 삭제, file 저장 경로 변경)

--- fileupload/models.py
+++ fileupload/models.py
@@ -7,18 +7,16 @@ from django.utils.decorators import method_decorator
 
 # rawdata entity
 class RawData(models.Model):
-  file_name = models.FileField(null=True, upload_to="data/%Y/%m/%d", blank=True)
+  file_name = models.FileField(null=True, upload_to="data/%Y/%m/%d", unique = True)
   describe = models.TextField(max_length=40, null=True)
-  created_at = models.DateTimeField(auto_now_add=True)
 
 # output entity
 class Output(models.Model):
   raw_data_id = models.ForeignKey("RawData", related_name="analysis", on_delete=models.CASCADE, db_column="raw_data_id")
-  file_name = models.FileField(null=True, upload_to="data/%Y/%m/%d", blank=True)
-  created_at = models.DateTimeField(auto_now_add=True)
-  analysis_tech = models.CharField(
-    max_length=40,
-    choices=[(tech.name, tech.value) for tech in AnalysisTech_forServer],
-    default=AnalysisTech_forServer.LinearRegression.value,
-  )
+  file_name = models.FileField(null=True, upload_to="result/%Y/%m/%d", unique= True)
+  # analysis_tech = models.CharField(
+  #   max_length=40,
+  #   choices=[(tech.name, tech.value) for tech in AnalysisTech_forServer],
+  #   default=AnalysisTech_forServer.LinearRegression.value,
+  # )
 
